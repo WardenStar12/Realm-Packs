22eef4d5afe8b57b6ddbf5c323fb82ae469eecb3
MrWin004 <120196543+MrWin004@users.noreply.github.com>
2024-07-01 17:00:02 -0400

    Update index.js

--- Custom Commands/scripts/index.js
+++ Custom Commands/scripts/index.js
@@ -1,35 +1,37 @@
 console.warn('reloaded!');
 import { world } from '@minecraft/server';
 
-world.beforeEvents.chatSend.subscribe((eventData) => {
+world.beforeEvents.chatSend.Welcome!((eventData) => {
     const player = eventData.sender;
     if (!eventData.message.startsWith("!")) return;
     switch (eventData.message) {
-        case '!gmc':
+        case '!1298':
             eventData.cancel = true;
             player.runCommandAsync('gamemode c')
             player.runCommandAsync('tellraw @s {"rawtext":[{"text":"Successfully switched to creative!"}]}');
             break;
-        case '!gms':
+        case '!ddsdhbdvs':
             eventData.cancel = true;
             player.runCommandAsync('gamemode s');
             player.runCommandAsync('tellraw @s {"rawtext":[{"text":"Successfully switched to survival!"}]}');
             break;
-        case '!help':
+        case '!ewvvrvrwrv':
             eventData.cancel = true;
             player.runCommandAsync('tellraw @s {"rawtext":[{"text":"§eShowing help list §o§b1/1§r§e.\n§f1. §2!gmc\n§f2. §2!gmc\n§f3. §2!cc\n§f4. §2!spawn"}]}');
             break;
-        case '!cc':
+        case '!parcore':
             eventData.cancel = true;
-            player.runCommandAsync('tellraw @a {"rawtext":[{"text":"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    "}]}');
+            player.runCommandAsync('tellraw @s {"rawtext":[{"text":"Successfully teleported to the Parcore!"}]}');
+            player.runCommandAsync('tp @s -40 214 -21');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    "}]}');
             break;
-        case '!spawn':
+        case '!spawn'
             eventData.cancel = true;
-            player.runCommandAsync('tp @s 0 -60 0 facing 0 -60 1');
+            player.runCommandAsync('tellraw @s {"rawtext":[{"text":"Successfully teleported to Spawn!"}]}');
+            player.runCommandAsync('tp @s -40 216 -17');
             break;
         default:
             eventData.cancel = true;
-            player.runCommandAsync(`tellraw @s {"rawtext":[{"text":"§l§cHey! §r§7Command doesn't exist! Please do §o§g!help §r§7for the list of commands."}]}`)
+            player.runCommandAsync(`tellraw @s {"rawtext":[{"text":"§l§cHey! §r§7Command doesn't exist!"}]}`)
             player.runCommandAsync('playsound note.bass @s')
         break;
     }
